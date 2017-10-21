ede9754ea49644a2fc70cb937c4e34d6bd76ae70
Dave Mount <dave@happyspiritgames.com>
2017-10-21 06:19:28 -0700

    some stuff

--- api/models/storyModel.js
+++ api/models/storyModel.js
@@ -6,8 +6,12 @@ exports.getStories = function() {
     console.log('looking for stories');
     var stories = [];
     fs.readdir('./storyRepo', function(err, items) {
-        console.log('found:', items);
+        for(var i = 0; i < items.length; i++) {
+            var story = fs.load(items[0] + '/story.json');
+            stories.push(story);
+        }
     });
+    return stories;
 };
 
 exports.getStory = function(storyKey) {
