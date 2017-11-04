0c58b828a2b683d45849506e726333c85c9345b9
Dave Mount <dave@happyspiritgames.com>
2017-11-04 08:26:47 -0700

    upgrade to node 8.9; consolidate into single repo.

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import ReaderPage from './pages/ReaderPage';
+
+class App extends Component {
+  render() {
+    return (
+      <ReaderPage />
+    );
+  }
+}
+
+export default App;
