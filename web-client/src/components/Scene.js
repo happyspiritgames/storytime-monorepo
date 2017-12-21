e0100eeb0f150cd68954f9c04b6d987e56c6b31a
Dave Mount <dave@happyspiritgames.com>
2017-12-21 14:41:04 -0800

    Next set of changes

--- web-client/src/components/Scene.js
+++ web-client/src/components/Scene.js
@@ -3,11 +3,14 @@ import { Card, CardText, CardBody, CardTitle } from 'reactstrap';
 
 const Scene = props => {
   const { title, prose } = props.scene;
+  const formatted = prose.split('\n').map(paragraph => {
+    return <div>{ paragraph }</div>;
+  });
   return (
     <Card id="scene">
       <CardBody>
         <CardTitle>{ title }</CardTitle>
-        <CardText>{ prose }</CardText>
+        <CardText>{ formatted }</CardText>
       </CardBody>
     </Card>
   );
