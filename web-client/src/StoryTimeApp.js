111b7faf884cc85a21b9bc392a75ce52069e30eb
Dave Mount <dave@happyspiritgames.com>
2018-01-01 15:52:44 -0800

    Get Facebook login/logout working (sort of).

--- web-client/src/StoryTimeApp.js
+++ web-client/src/StoryTimeApp.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Library from './components/Library';
 import Reader from './components/Reader';
+import Settings from './components/Settings';
 
 const StoryTimeApp = () => (
   <div>
-    <Route exact path="/" component={Library} />
+    <Route exact path="/" component={Settings} />
     <Route path="/library" component={Library} />
     <Route path="/reader/:storyKey" component={Reader} />
   </div>
