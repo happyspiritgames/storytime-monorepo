3f0f396ead077301656d0a14ca800631e311ec1e
Dave Mount <dave@happyspiritgames.com>
2018-01-10 10:45:46 -0800

    Tidy up

--- web-client/src/config.js
+++
@@ -1,6 +0,0 @@
-export const CLIENT_IDS = {
-  google: {
-    clientId: '611755432334-tpl4ejet5kubkn6tcch7ci0ogigajtg8.apps.googleusercontent.com',
-    secretKey: 'dJsXZN0y-9Twz4ai7E71lel8'
-  }
-};
