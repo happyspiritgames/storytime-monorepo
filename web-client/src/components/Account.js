2c799fa51de1d260619c5c3815854bffe8e2dac9
Dave Mount <dave@happyspiritgames.com>
2018-01-10 10:13:26 -0800

    Got Google login working with react-google-login module

--- web-client/src/components/Account.js
+++ web-client/src/components/Account.js
@@ -1,40 +1,74 @@
 import React, { Component } from 'react';
 import { Container } from 'reactstrap';
-import GoogleSignIn from './login/GoogleSignIn';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
 import MemberInfoCard from './MemberInfoCard';
-
-const memberProps = {
-  email: 'bubba@happyspiritgames.com',
-  nickname: 'Bubba',
-  membersOnlyComms: true
-};
 
 export default class Account extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      isLoggedIn: false
+      isLoggedIn: false,
+      memberProps: {}
     };
   }
 
-  render() {
-    let pageHeading;
-    let loginButtons;
-    let memberInfo;
+  handleGoogleLogin = (googleUser) => {
+    const profile = googleUser.getBasicProfile();
+    console.log('handleGoogleLogin success', googleUser);
+    console.log('basic profile', profile);
+    const memberProps = {
+      email: profile.getEmail(),
+      nickname: profile.getName(),
+      membersOnlyComms: true,
+      profilePicUrl: profile.getImageUrl()
+    };
+    this.setState({
+      isLoggedIn: true,
+      memberProps: memberProps
+    })
+  }
 
+  handleGoogleLogout = (result) => {
+    console.log('handleGoogleLogout', result);
+    this.setState({
+      isLoggedIn: false
+    });
+  }
+
+  handleGoogleFailure = (error) => {
+    console.log('handleGoogleFailure error', error);
+  }
+
+  renderAuthButtons() {
     if (this.state.isLoggedIn) {
-      pageHeading = 'All About You';
-      memberInfo = <MemberInfoCard {...memberProps}/>;
+      return (
+        <GoogleLogout
+          buttonText="Logout of Google"
+          onLogoutSuccess={this.handleGoogleLogout}
+        />
+      );
     } else {
-      pageHeading = 'Who Are You?';
-      loginButtons = <GoogleSignIn />;
+      return (
+        <GoogleLogin
+          clientId="611755432334-tpl4ejet5kubkn6tcch7ci0ogigajtg8.apps.googleusercontent.com"
+          buttonText="Login w/Google"
+          onSuccess={this.handleGoogleLogin}
+          onFailure={this.handleGoogleFailure}
+        />
+      );
     }
+  }
+
+  render() {
+    const pageHeading = (this.state.isLoggedIn) ? 'All About You' : 'Who Are You?';
+    const memberInfo = (!this.state.isLoggedIn) ? undefined :
+      <MemberInfoCard {...this.state.memberProps}/>;
 
     return (
       <Container id="account" fluid={true} className="storytime-page">
         <h1 className="text-center">{pageHeading}</h1>
-        {loginButtons}
+        {this.renderAuthButtons()}
         {memberInfo}
       </Container>
     );
