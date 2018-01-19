26de361770d951e9aae743ea48f44f03e092cd14
Dave Mount <dave@happyspiritgames.com>
2018-01-19 10:50:42 -0800

    Updates to schema, associated queries.

--- src/persistence/postgres/storytime-inserts.sql
+++ src/persistence/postgres/storytime-inserts.sql
@@ -1,46 +1,67 @@
 
--- create account
-INSERT INTO member
-(id, email, nickname, facebook_id, google_id, agree_members_only_comms)
-VALUES ('b8de254b-6a98-468b-bfdb-51c27506e4b0', 'bubba@happyspiritgames.com',
-  'bubba gump', '??facebook??', '??google??', NOW());
+-- create player
+BEGIN
 
--- update email
-UPDATE member
-SET email = $2
-WHERE id = $1
+INSERT INTO player
+(id, idp_sub, email, nickname)
+VALUES ($1, $2, $3, $4)
+RETURNING id; // ?select on the row?
 
--- update nickname
-UPDATE member
+INSERT INTO identity
+(idp_sub, player_id, idp_profile)
+VALUE ($1, $2, $3);  // pull $2 from return of first insert
+
+COMMIT
+
+see https://node-postgres.com/features/transactions
+use async/await
+
+-- create player example
+INSERT INTO player
+(id, idp_sub, email, nickname)
+VALUES ('b8de254b-6a98-468b-bfdb-51c27506e4b0', 'google|10214121106678290',
+  'bubba@happyspiritgames.com', 'BubbaBubbaGump');
+
+INSERT INTO identity
+(idp_sub, player_id, idp_profile)
+VALUE ('google|10214121106678290', 'b8de254b-6a98-468b-bfdb-51c27506e4b0',
+  '{}');
+
+-- look up player given idp_sub
+SELECT player_id
+FROM identity
+WHERE ipd_sub = "";
+
+-- look up player info
+SELECT created_at, email, nickname, agreed_to_comms_at
+FROM player
+WHERE id = "";
+
+-- update player info
+UPDATE player
 SET nickname = $2
-WHERE id = $1
-
--- login facebook
-UPDATE member
-SET facebook_id = $2
-WHERE id = $1
-
--- forget facebook login (logout?)
-UPDATE member
-SET facebook_id = DEFAULT
-WHERE id = $1
-
--- login google
-UPDATE member
-SET google_id = $2
-WHERE id = $1
-
--- forget google login (logout?)
-UPDATE member
-SET google_id = DEFAULT
-WHERE id = $1
+WHERE id = $1;
 
 -- agree to communications
-UPDATE member
-SET agree_members_only_comms = NOW()
-WHERE id = $1
+UPDATE player
+SET agreed_to_comms_at = NOW()
+WHERE id = $1;
 
--- refuse communications
-UPDATE member
-SET agree_members_only_comms = DEFAULT
-WHERE id = $1
+-- revoke agreement to communications
+UPDATE player
+SET agreed_to_comms_at = DEFAULT
+WHERE id = $1;
+
+-- look up stored profile
+SELECT idp_profile
+FROM identity
+WHERE idp_sub = $1
+
+-- update profile
+UPDATE identity
+SET idp_profile = $2
+WHERE idp_sub = $1;
+
+-- delete a player
+DELETE FROM identity WHERE player_id = $1;
+DELETE FROM player WHERE id = $1;
--- src/persistence/postgres/storytime-schema.sql
+++ src/persistence/postgres/storytime-schema.sql
@@ -1,34 +1,13 @@
-CREATE TABLE member (
+CREATE TABLE player (
   id UUID PRIMARY KEY,
-  created timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   email varchar(128) NOT NULL,
-  nickname varchar(64) NOT NULL,
-  facebook_id varchar(128),
-  facebook_user_info text,
-  google_id varchar(128),
-  google_user_info text,
-  agree_members_only_comms timestamp,
-  agree_author_terms timestamp,
-  marked_for_archive timestamp
+  nickname varchar(64) NOT NULL DEFAULT "Rising Star",
+  agreed_to_comms_at timestamp
 );
 
-CREATE TABLE identity_provider (
-  id integer PRIMARY KEY,
-  provider_key varchar(48),
-  provider_name varchar(48)
-)
-
-// one record for each user-identity_provider combination;
-// store everything received from provider as JSON in member_info;
-// delete member_token (?) when logged out;
-// delete record when asked to be forgotten
-// TODO figure out how to remember that someone left -- event log?
 CREATE TABLE identity (
-  member_id UUID, (member_fk)
-  identity_provider_id integer,
-  registered_at timestamp,
-  member_token varchar(64),
-  profile_info text
-)
-
-// google profile from ID token includes: email, emailVerified (bool), name, picture, locale, familyName, givenName
+  idp_sub varchar(64) PRIMARY KEY,
+  player_id UUID REFERENCES player,
+  idp_profile text
+);
