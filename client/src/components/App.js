beef12bf27413496b25ae71ed05da563d6536637
Dave Mount <dave@happyspiritgames.com>
2018-02-14 15:23:07 -0800

    Revamped about page.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
-import Clubhouse from './Clubhouse';
 import Library from './library';
 import Reader from './reader';
 import Account from './account';
+import Admin from './admin';
+import About from './about';
 import LoginCallback from './account/LoginCallback';
-import Admin from './admin/PlayerAdmin';
 
 export default class StoryTimeApp extends Component {
 
@@ -16,6 +16,7 @@ export default class StoryTimeApp extends Component {
         <Route path="/reader/:storyId/:sceneId" component={Reader} />
         <Route path="/account" component={Account} />
         <Route path="/admin" component={Admin} />
+        <Route path="/about" component={About} />
         <Route path="/callback" component={LoginCallback} />
       </div>
     );
