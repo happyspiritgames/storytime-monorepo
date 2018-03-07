3bb3b015dc5db8e18f4889fe8e700873596b52c8
Dave Mount <dave@happyspiritgames.com>
2018-03-07 15:42:17 -0800

    Provide defaults for signpost prompt.

--- client/src/components/reader/Signpost.js
+++ client/src/components/reader/Signpost.js
@@ -3,15 +3,21 @@ import PropTypes from 'prop-types'
 import Sign from './Sign'
 import { sceneShape } from '../../datastore/dataShapes'
 
+const DEFAULT_PROMPT = 'What would you like to do?'
+const DEFAULT_ENDING_PROMPT = 'The End'
+
 const Signpost = ({ scene, goToScene, playAgain, goToLibrary }) => {
   const { endPrompt, signpost } = scene
   let signs
+  let prompt
   if (signpost) {
+    prompt = endPrompt || DEFAULT_PROMPT
     signs = signpost.map(sign => {
       let signKey = `${sign.sceneId}|${sign.teaser}`
       return (<Sign key={signKey} onClick={() => { goToScene(sign.sceneId) }} text={sign.teaser} />)
     })
   } else {
+    prompt = endPrompt || DEFAULT_ENDING_PROMPT
     signs = [
       <Sign key='replay' onClick={() => { playAgain() }} text='Go back to the beginning and try again.' />,
       <Sign key='feedback' onClick={() => { console.log('implement feedback') }} text='Give some feedback.' />,
@@ -21,7 +27,7 @@ const Signpost = ({ scene, goToScene, playAgain, goToLibrary }) => {
   return (
     <div className="card">
       <div className="card-header prompt">
-        <h5 className="mb-0">{endPrompt}</h5>
+        <h5 className="mb-0">{prompt}</h5>
       </div>
       <div className="card-body">
         <ul className="list-group">
