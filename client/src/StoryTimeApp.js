940cd898beae544fda0ab596de1adfff068384a2
Dave Mount <dave@happyspiritgames.com>
2018-03-08 10:50:25 -0800

    Added an error boundary.

--- client/src/StoryTimeApp.js
+++ client/src/StoryTimeApp.js
@@ -3,11 +3,12 @@ import { Route, Switch } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { ConnectedRouter } from 'react-router-redux'
 import store, { history } from './datastore/store'
+import ErrorBoundaryContainer from './components/ErrorBoundaryContainer'
 import LibraryPage from './components/library'
 import ReaderPage from './components/reader'
 import LoginCallback from './components/navigation/LoginCallback'
 import About from './components/about'
-import Account from './components/account'
+import AccountPage from './components/account'
 import Admin from './components/admin'
 import Contact from './components/contact'
 import Footer from './components/footer'
@@ -22,26 +23,28 @@ export default class StoryTimeApp extends Component {
   render() {
     return (
       <Provider store={store}>
-        <ConnectedRouter history={history}>
-          <div className="container-fluid">
-            <Navigation />
-            <div className="main-content">
-              <Switch>
-                <Route exact path="/" component={LibraryPage} />
-                <Route path="/about" component={About} />
-                <Route path="/account" component={Account} />
-                <Route path="/admin" component={Admin} />
-                <Route path="/callback" component={LoginCallback} />
-                <Route path="/contact" component={Contact} />
-                <Route path="/reader/:storyId" component={ReaderPage} />
-                <Route exact path="/writingdesk" component={WritingDesk} />
-                <Route exact path="/writingdesk/:draftId" component={EditStory} />
-                <Route path="/writingdesk/:draftId/:sceneId" component={EditScene} />
-              </Switch>
+        <ErrorBoundaryContainer>
+          <ConnectedRouter history={history}>
+            <div className="container-fluid">
+              <Navigation />
+              <div className="main-content">
+                <Switch>
+                  <Route exact path="/" component={LibraryPage} />
+                  <Route path="/about" component={About} />
+                  <Route path="/account" component={AccountPage} />
+                  <Route path="/admin" component={Admin} />
+                  <Route path="/callback" component={LoginCallback} />
+                  <Route path="/contact" component={Contact} />
+                  <Route path="/reader/:storyId" component={ReaderPage} />
+                  <Route exact path="/writingdesk" component={WritingDesk} />
+                  <Route exact path="/writingdesk/:draftId" component={EditStory} />
+                  <Route path="/writingdesk/:draftId/:sceneId" component={EditScene} />
+                </Switch>
+              </div>
+              <Footer />
             </div>
-            <Footer />
-          </div>
-        </ConnectedRouter>
+          </ConnectedRouter>
+        </ErrorBoundaryContainer>
       </Provider>
     )
   }
