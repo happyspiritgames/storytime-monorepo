fe2cadbbd927d72621c1b9161f6d587119296bb0
Dave Mount <dave@happyspiritgames.com>
2018-03-15 07:57:19 -0700

    Finished adding methods to authorApi for 1.4.

--- client/src/apis/authorApi.js
+++ client/src/apis/authorApi.js
@@ -4,7 +4,8 @@ const draftStoriesBaseURI = '/api/draft-stories'
 
 export const fetchDraftStories = (handleDraftStories, handleError) => {
   console.log('called getDraftStories')
-  fetch(draftStoriesBaseURI, { headers: getHeaders() })
+  const fetchOptions = { headers: getHeaders() }
+  fetch(draftStoriesBaseURI, fetchOptions)
     .then(res => res.json())
     .then(stories => handleDraftStories(stories))
     .catch(error => handleError(error))
@@ -12,7 +13,8 @@ export const fetchDraftStories = (handleDraftStories, handleError) => {
 
 export const fetchFullDraft = (storyId, handleDraft, handleError) => {
   console.log('fetchFullDraft', storyId)
-  fetch(`${draftStoriesBaseURI}/${storyId}/full`, { headers: getHeaders() })
+  const fetchOptions = { headers: getHeaders() }
+  fetch(`${draftStoriesBaseURI}/${storyId}/full`, fetchOptions)
     .then(res => res.json())
     .then(draft => handleDraft(draft))
     .catch(error => handleError(error))
@@ -20,12 +22,12 @@ export const fetchFullDraft = (storyId, handleDraft, handleError) => {
 
 export const createDraft = (draftSummary, handleResponse, handleError) => {
   console.log('createDraft: summary', draftSummary)
-  const postOptions = {
+  const fetchOptions = {
     method: 'POST',
     headers: getHeaders(),
     body: JSON.stringify(draftSummary)
   }
-  fetch(draftStoriesBaseURI, postOptions)
+  fetch(draftStoriesBaseURI, fetchOptions)
     .then(res => res.json())
     .then(summaryResponse => handleResponse(summaryResponse))
     .catch(error => handleError(error))
@@ -33,13 +35,74 @@ export const createDraft = (draftSummary, handleResponse, handleError) => {
 
 export const updateDraft = (draftSummary, handleResponse, handleError) => {
   console.log('updateDraft: summary', draftSummary)
-  const putOptions = {
+  const fetchOptions = {
     method: 'PUT',
     headers: getHeaders(),
     body: JSON.stringify(draftSummary)
   }
-  fetch(`${draftStoriesBaseURI}/${draftSummary.storyId}`, putOptions)
+  fetch(`${draftStoriesBaseURI}/${draftSummary.storyId}`, fetchOptions)
     .then(res => res.json())
     .then(summaryResponse => handleResponse(summaryResponse))
     .catch(error => handleError(error))
+}
+
+export const fetchScene = (storyId, sceneId, handleDraft, handleError) => {
+  console.log('fetchScene', storyId, sceneId)
+  const fetchOptions = { headers: getHeaders() }
+  fetch(`${draftStoriesBaseURI}/${storyId}/scenes/${sceneId}`, fetchOptions)
+    .then(res => res.json())
+    .then(draft => handleDraft(draft))
+    .catch(error => handleError(error))
+}
+
+export const createScene = (storyId, draftScene, handleResponse, handleError) => {
+  console.log('createScene: summary', draftScene)
+  const fetchOptions = {
+    method: 'POST',
+    headers: getHeaders(),
+    body: JSON.stringify(draftScene)
+  }
+  fetch(`draftStoriesBaseURI/${storyId}/scenes`, fetchOptions)
+    .then(res => res.json())
+    .then(summaryResponse => handleResponse(summaryResponse))
+    .catch(error => handleError(error))
+}
+
+export const updateScene = (storyId, draftScene, handleResponse, handleError) => {
+  console.log('updateScene: summary', draftScene)
+  const fetchOptions = {
+    method: 'PUT',
+    headers: getHeaders(),
+    body: JSON.stringify(draftScene)
+  }
+  fetch(`${draftStoriesBaseURI}/${storyId}/scenes/${draftScene.sceneId}`, fetchOptions)
+    .then(res => res.json())
+    .then(summaryResponse => handleResponse(summaryResponse))
+    .catch(error => handleError(error))
+}
+
+export const fetchSignpost = (storyId, sceneId, handleDraft, handleError) => {
+  console.log('fetchSignpost', storyId, sceneId)
+  const fetchOptions = { headers: getHeaders() }
+  fetch(`${draftStoriesBaseURI}/${storyId}/scenes/${sceneId}/signpost`, fetchOptions)
+    .then(res => res.json())
+    .then(draft => handleDraft(draft))
+    .catch(error => handleError(error))
+}
+
+export const updateSignpost = (storyId, sceneId, signpost, handleResponse, handleError) => {
+  console.log('updateSignpost: summary', storyId, sceneId, signpost)
+  const fetchOptions = {
+    method: 'PUT',
+    headers: getHeaders(),
+    body: JSON.stringify(signpost)
+  }
+  fetch(`${draftStoriesBaseURI}/${storyId}/scenes/${sceneId}/signpost`, fetchOptions)
+    .then(res => res.json())
+    .then(summaryResponse => handleResponse(summaryResponse))
+    .catch(error => handleError(error))
+}
+
+export const publish = (storyId, handleResponse, handleError) => {
+  console.log('implementing publish in the next release')
 }
