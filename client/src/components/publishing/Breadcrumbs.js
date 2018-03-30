5a570d150d3e876a818434005dc40cf25f9e015d
Dave Mount <dave@happyspiritgames.com>
2018-03-30 11:09:25 -0700

    Things are mostly wired up locally on Publishing page.

--- client/src/components/publishing/Breadcrumbs.js
+++ client/src/components/publishing/Breadcrumbs.js
@@ -4,6 +4,9 @@ import { Link } from 'react-router-dom'
 function Breadcrumbs(props) {
   const { proof } = props
   const crumbs = []
+
+  // TODO make this work for publishing page
+  
   crumbs.push(<li className="breadcrumb-item"><Link to="/writingdesk">Projects</Link></li>)
   if (proof && proof.title) {
     crumbs.push(<li className="breadcrumb-item">{proof.title}</li>)
--- client/src/components/publishing/index.js
+++ client/src/components/publishing/index.js
@@ -13,8 +13,22 @@ export default class Publishing extends Component {
 
   state = { storyKey: 'blargy', rating: '14' }
 
+  handleChange = (event) => {
+    this.setState({
+      [event.target.id]: event.target.value
+    })
+  }
+
   save = () => {
     console.log('clicked save')
+  }
+
+  publish = () => {
+    console.log('clicked publish')
+  }
+
+  playStory = () => {
+    console.log('clicked play story')
   }
 
   render() {
@@ -42,14 +56,14 @@ export default class Publishing extends Component {
                     <div className="form-group">
                       <label>Story Unique Key</label>
                       <small className="form-text text-muted">Use the random key or invent something more fun.</small>
-                      <input className="form-control" type="text" id="storyKey" value={this.state.storyKey} />
+                      <input className="form-control" type="text" id="storyKey" value={this.state.storyKey} onChange={this.handleChange}/>
                     </div>
                   </div>
                   <div className="col-6">
                     <div className="form-group">
                       <label>Content Rating</label>
                       <small className="form-text text-muted">Age group for whom your story-game is appropriate.</small>
-                      <select className="form-control" id="rating" value={this.state.rating}>
+                      <select className="form-control" id="rating" value={this.state.rating} onChange={this.handleChange}>
                         <optgroup label="This is a group">
                           <option value="Y">Youth</option>
                           <option value="Y7">Youth Age 7+</option>
@@ -68,25 +82,25 @@ export default class Publishing extends Component {
                     <div className="col-3">
                       <div className="form-check">
                         <input className="form-check-input" type="checkbox" id="genre-adventure" />
-                        <label className="form-check-label" for="genre-adventure">Adventure</label>
+                        <label className="form-check-label" htmlFor="genre-adventure">Adventure</label>
                       </div>
                     </div>
                     <div className="col-3">
                       <div className="form-check">
                         <input className="form-check-input" type="checkbox" id="genre-fantasy" />
-                        <label className="form-check-label" for="genre-fantasy">Fantasy</label>
+                        <label className="form-check-label" htmlFor="genre-fantasy">Fantasy</label>
                       </div>
                     </div>
                     <div className="col-3">
                       <div className="form-check">
                         <input className="form-check-input" type="checkbox" checked="" id="genre-scifi" />
-                        <label className="form-check-label" for="genre-scifi">Science Fiction</label>
+                        <label className="form-check-label" htmlFor="genre-scifi">Science Fiction</label>
                       </div>
                     </div>
                     <div className="col-3">
                       <div className="form-check">
                         <input className="form-check-input" type="checkbox" id="genre-romance" />
-                        <label className="form-check-label" for="genre-romance">Romance</label>
+                        <label className="form-check-label" htmlFor="genre-romance">Romance</label>
                       </div>
                     </div>
                   </div>
@@ -94,25 +108,25 @@ export default class Publishing extends Component {
                     <div className="col-3">
                       <div className="form-check">
                         <input className="form-check-input" type="checkbox" id="genre-drama" />
-                        <label className="form-check-label" for="genre-drama">Drama</label>
+                        <label className="form-check-label" htmlFor="genre-drama">Drama</label>
                       </div>
                     </div>
                     <div className="col-3">
                       <div className="form-check">
                         <input className="form-check-input" type="checkbox" id="genre-mystery" />
-                        <label className="form-check-label" for="genre-mystery">Mystery</label>
+                        <label className="form-check-label" htmlFor="genre-mystery">Mystery</label>
                       </div>
                     </div>
                     <div className="col-3">
                       <div className="form-check">
                         <input className="form-check-input" type="checkbox" id="genre-satire" />
-                        <label className="form-check-label" for="genre-satire">Satire</label>
+                        <label className="form-check-label" htmlFor="genre-satire">Satire</label>
                       </div>
                     </div>
                     <div className="col-3">
                       <div className="form-check">
                         <input className="form-check-input" type="checkbox" id="genre-historical" />
-                        <label className="form-check-label" for="genre-historical">Historical</label>
+                        <label className="form-check-label" htmlFor="genre-historical">Historical</label>
                       </div>
                     </div>
                   </div>
@@ -120,7 +134,7 @@ export default class Publishing extends Component {
                     <div className="col-3">
                       <div className="form-check">
                         <input className="form-check-input" type="checkbox" id="genre-biography" />
-                        <label className="form-check-label" for="genre-biography">Biographical</label>
+                        <label className="form-check-label" htmlFor="genre-biography">Biographical</label>
                       </div>
                     </div>
                   </div>
@@ -128,6 +142,34 @@ export default class Publishing extends Component {
                 <button className="btn btn-primary" type="button" onClick={this.save}>Save</button>
               </fieldset>
             </form>
+            <h4 className="text-center">Proof</h4>
+            <div className="row">
+              <div className="col">
+                <p>This is how your story-game will appear in the catalog. If it looks good, you are all set.</p>
+                <p>To make changes, you have to return to the draft.</p>
+                <p><Link to={`/writingdesk/${draftId}`}>Return to story editor.</Link></p>
+              </div>
+              <div className="col">
+                <div className="card">
+                  <img className="card-img-top w-100 d-block" src="" alt="" />
+                  <div className="card-body">
+                    <h4 className="card-title"><em>My First Story</em></h4>
+                    <h6 className="card-subtitle">by Bubba Gump</h6>
+                    <p className="card-text">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p>
+                    <button className="btn btn-primary" type="button" onClick={this.playStory}>Play</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col">
+                <h4 className="text-center">Publish</h4>
+                <p>Your story has not been published.</p>
+                <p>Your story was published on March 31, 2018 at 4:30 PM.</p>
+                <button className="btn btn-primary" type="button" onClick={this.publish}>Publish Now - Do It - Make it so!</button>
+                <p>(Note: publishing happens once. After that, classifications can change, but you cannot re-publish. Can always go through the publishing cycle again and publish a new version. Should make it easy to delete non-current published versions.)</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
